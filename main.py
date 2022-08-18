1ef1f5e5814ae9b7a9956de3f9d5fce9e008a070
Loryn <lorynhaykus@yahoo.com>
2022-08-17 20:44:21 -0600

    got car drawn in track, creating rotation funcitn for moving car

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import pygame
 import time
 import math
 #importing resizing image funciton from utils file
-from utils import scale_image
-
-
+from utils import scale_image,blit_rotate_center
 
 #create variable and load in images
 #GRASS = constant(not going to change)
@@ -23,8 +21,8 @@ TRACK_BORDER = scale_image(pygame.image.load('images/track-border.png'), 0.9)
 FINISH = pygame.image.load('images/finish.png')
 
 #load car images
-PURPLE_CAR = pygame.image.load('images/purple-car.png')
-WHITE_CAR = pygame.image.load('images/white-car.png')
+PURPLE_CAR = scale_image(pygame.image.load('images/purple-car.png'), 0.55 )
+WHITE_CAR = scale_image(pygame.image.load('images/white-car.png'), 0.55)
 
 #set up display screen to be same size as width and height as track
 # get the width and height from track image
@@ -37,6 +35,46 @@ pygame.display.set_caption("Racing Game!")
 #setting frames per second
 FPS = 60
 
+# Setting up CLASS
+# Abstract class - there will be a player car and a computer car, similiarities between the two will go in an abstract class
+#Abstract class = meant to act as base class for other classes
+class AbstractCar:
+   
+    # wanting to know max velocity and how quickly car can rotate
+    def __init__(self, max_vel, rotation_vel) :
+        self.img = self.IMG
+        self.max_vel = max_vel
+        #starting velocity = 0 because car is not moving 
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        # X and Y position of car
+        self.x, self.y = self.START_POS
+#creating funciton to rotate car based on direction turning
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+
+#Creating new class for PlayerCar, refering and using code from AbstractCar, setting player image 
+class PlayerCar(AbstractCar):
+    #setting class image
+    IMG = PURPLE_CAR
+    START_POS = (180, 200)
+
+
+#draw funciton takes window to draw on and images you want to draw
+def draw(win, images, player_car):
+    for img, pos in images:
+        win.blit(img, pos)
+
+    player_car.draw(win)
+    pygame.display.update()
+
 #EVENT LOOPS
 # Creating an event loop - event loop is a constant loop that handles all movement, events, etc.
 #Set up main event loop to keep game 'alive' and running on screen adn then once game ends or user quits
@@ -47,17 +85,17 @@ run = True
 # pygames run automatically depending on processor of computer, creating a clock lets playeres play at same speed
 #regardless of how slow/fast their processor is
 clock = pygame.time.Clock()
+#Creating list of images and what coordinates to draw them
+images = [(GRASS, (0,0)), (TRACK, (0,0))]
+#set player car as playercar class, pass max veolcity and rotation velocity
+player_car = PlayerCar(4, 4)
 while run:
     #setting max FPS that the while loop can run 
     clock.tick(FPS)
 
-    #blit- used to draw image on screen
-    #drawing image on window display screen
-    # 0,0 = top left corner, as you go down screen Y increases, X increases as you go right
-   #order of images drawn matter = GRASS then TRACK = track drawn over grass
-    WIN.blit(GRASS, (0,0))
-    WIN.blit(TRACK, (0,0))
-    WIN.blit(FINISH, (0,0))
+    #draw in images
+    draw(WIN, images, player_car)
+
 
     #pygame allows items to be drawn onto screen and when this is run
     # what was drawn on screen will then be displayed 
--- utils.py
+++ utils.py
@@ -8,3 +8,22 @@ def scale_image(img, factor):
     size = round(img.get_width() * factor), round(img.get_height() * factor)
     #returning the img as the new size
     return pygame.transform.scale(img, size)
+
+#Function to retrun rotated image based on angles
+#Pygame is a rectangle and images are essentially rectangles within
+#When rotating just the rectangle shape, morphing and dissortions can occur
+def blit_rotate_center(win, image, top_left, angle):
+    #rotate from top left 
+    rotated_image = pygame.transform.rotate(image, angle)
+    #rotate image without changing X,Y position of image on screen
+    #get top left positon of original image, get the center of that 
+    #new rectangle is = to the center of original image, so new image is rotating from center not top left 
+    new_rect = rotated_image.get_rect(
+        center=image.get_rect(topleft=top_left).center)
+    win.blit(rotated_image, new_rect.topleft)
+    #find X,Y of new rectangle 
+    win.blit(rotated_image, new_rect.topleft)
+
+   
+  
+    
