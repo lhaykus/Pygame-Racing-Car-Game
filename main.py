84373411edda4ed04cd0450a8c45b9870d1a054d
Loryn <lorynhaykus@yahoo.com>
2022-08-17 20:01:17 -0600

    got grass image to be drawn onto display and also using clock to create max FPS for game to be run

--- main.py
+++ main.py
@@ -29,13 +29,32 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 #naming window
 pygame.display.set_caption("Racing Game!")
 
-#DISPLAYING images
+#setting frames per second
+FPS = 60
+
+#EVENT LOOPS
 # Creating an event loop - event loop is a constant loop that handles all movement, events, etc.
 #Set up main event loop to keep game 'alive' and running on screen adn then once game ends or user quits
 # the screen, the loop is broken
 
 run = True
+# CLOCK - set up clock so that window is run at same speed on every computer
+# pygames run automatically depending on processor of computer, creating a clock lets playeres play at same speed
+#regardless of how slow/fast their processor is
+clock = pygame.time.Clock()
 while run:
+    #setting max FPS that the while loop can run 
+    clock.tick(FPS)
+
+    #blit- used to draw image on screen
+    #drawing image on window display screen
+    # 0,0 = top left corner, as you go down screen Y increases, X increases as you go right
+    WIN.blit(GRASS, (0,0))
+
+    #pygame allows items to be drawn onto screen and when this is run
+    # what was drawn on screen will then be displayed 
+    pygame.display.update()
+
     #get list of all events to loop through
     for event in pygame.event.get():
         #check to see if user closed window
