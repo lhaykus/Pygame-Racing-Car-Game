174560dfbe1afffb90fddc59cfb7a4e9cdfc2c9b
Loryn <lorynhaykus@yahoo.com>
2022-08-17 19:42:13 -0600

    install pygame and creating variables for images

---
+++ main.py
@@ -0,0 +1,22 @@
+#importing packages
+import pygame
+import time
+import math
+
+#create variable and load in images
+#GRASS = constant(not going to change)
+#loading grass image from the images folder
+GRASS = pygame.image.load('images/grass.jpg')
+
+#loading track image from image folder
+TRACK = pygame.image.load('images/track.png')
+
+#loading track border image
+TRACK_BORDER = pygame.image.load('images/track-border.png')
+
+#load finish line image
+FINISH = pygame.image.load('images/finish.png')
+
+#load car images
+PURPLE_CAR = pygame.image.load('images/purple-car.png')
+WHITE_CAR = pygame.image.load('images/white-car.png')
